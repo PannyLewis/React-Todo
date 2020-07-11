9558b97fffae4417928bcafe633d108f3ae22bd6
Panny Lewis <pannylewis@gmail.com>
2020-07-11 01:55:26 -0500

    App.js: created todo array, added constructor, addItem, toggleItem, clearCompleted, rendering 2 components:  TodoForm and TodoList.  Todo.js: added ternary for completed task using classname item.  TodoList.js:  has Todo as child inside its div shopping-list, has the clear button.  TodoForm.js: has constructor, the handlechanges, submitItem, renders the add task button.

--- src/App.js
+++ src/App.js
@@ -1,13 +1,94 @@
-import React from 'react';
+import React from "react";
+import TodoList from "./components/TodoList";
+import TodoForm from "./components/TodoForm";
+
+const todos = [
+  {
+    task: "dishes",
+    id: 111,
+    completed: false,
+  },
+  {
+    task: "laundry",
+    id: 222,
+    completed: false,
+  },
+  {
+    task: "cooking",
+    id: 333,
+    completed: false,
+  },
+  {
+    task: "shopping",
+    id: 444,
+    completed: false,
+  },
+];
 
 class App extends React.Component {
-  // you will need a place to store your state in this component.
-  // design `App` to be the parent component of your application.
   // this component is going to take care of state, and any change handlers you need to work with your state
+
+  // you will need a place to store your state in this component.
+  constructor() {
+    super();
+    this.state = {
+      todos,
+    };
+  }
+
+  // add new item when user enters value into input field, the ellipses ... copy over the previous values
+  addItem = (e, item) => {
+    e.preventDefault();
+    const newItem = {
+      name: item,
+      id: Date.now(),
+      completed: false,
+    };
+    this.setState({
+      todos: [...this.state.todos, newItem],
+    });
+  };
+
+  // causes the font to change from normal to striked to look like it's been crossed off and done
+  toggleItem = (itemId) => {
+    console.log(itemId);
+    // map over array
+    // when we find the item we clicked, toggle the completed field
+    // otherwise return the item untouched
+    this.setState({
+      todos: this.state.todos.map((item) => {
+        if (itemId === item.id) {
+          return {
+            ...item,
+            completed: !item.completed,
+          };
+        }
+        return item;
+      }),
+    });
+  };
+
+  clearcompleted = (e) => {
+    e.preventDefault(); // prevents page from refreshing
+    // if item is completed (item.completed is true) then filter out
+    this.setState({
+      todos: this.state.todos.filter((item) => !item.completed),
+    });
+  };
+
+  // design `App` to be the parent component of your application.
   render() {
     return (
-      <div>
-        <h2>Welcome to your Todo App!</h2>
+      <div className="App">
+        <div className="header">
+          <h1>To Do List</h1>
+          <TodoForm addItem={this.addItem} />
+        </div>
+        <TodoList
+          todos={this.state.todos}
+          toggleItem={this.toggleItem}
+          clearcompleted={this.clearcompleted}
+        />
       </div>
     );
   }
